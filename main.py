0569f0e557136be6141538a504601aa449389969
O'tkirbek Alimov <99404246+Otkirbek10@users.noreply.github.com>
2023-05-26 23:18:26 +0500

    Starting project

---
+++ main.py
@@ -0,0 +1,17 @@
+from PIL import Image
+# import pytesseract
+
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# img = Image.open('yaxsh.jpg')
+# file_name = img.filename
+# file_name = file_name.split('.')[0]
+
+# custom_config = r'--oem 1 --psm 6'
+
+# text = pytesseract.image_to_string(img,config = custom_config)
+
+# with open (f'{file_name}.text','w') as text_file:
+#     text_file.write(text)
+
+# print(text)
